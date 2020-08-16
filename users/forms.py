1f811c6bfd3da0ac05a3cd1640e9a1122cc37553
Zeinab Elazab <zeinabelazzab875@gmail.com>
2020-08-16 16:17:44 +0200

    custom user model

---
+++ users/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+from.models import CustomUser
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        #fields = UserCreationForm.Meta.fields +("age",)
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = CustomUser
+        #fields = UserCreationForm.Meta.fields
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
